c08cb4a0da9271c1b542bd16c06fceff68a1c628
Pili_zr36 <77540303+Evgeniy959@users.noreply.github.com>
2023-09-26 22:05:17 +0700

    add

---
+++ Download/WPF_Image-master/WPF_Image-master/WPF_Image/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using Microsoft.Win32;
+
+namespace WPF_Image
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_OpenImage_OnClick(object sender, RoutedEventArgs e)
+        {
+            var path = new Uri(OpenFileDialog());
+            Image.Source = new BitmapImage(path);
+        }
+
+        private string OpenFileDialog()
+        {
+            var openFileDialog = new OpenFileDialog();
+            
+            return openFileDialog.ShowDialog() == true ? openFileDialog.FileName : string.Empty;
+        }
+
+        private void Button_ChangeColor(object sender, RoutedEventArgs e)
+        {
+            var name = (sender as Button)?.Name;
+
+            Canvas.DefaultDrawingAttributes.Color = name switch
+            {
+                "Button_ColorBlack" => Colors.Black,
+                "Button_ColorRed" => Colors.Red,
+                _ => Canvas.DefaultDrawingAttributes.Color
+            };
+        }
+    }
+}
